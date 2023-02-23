5762c4dbb8c19c030014bb6bd5a221846a20ab1a
Brendamasingu <brendamasingu@gmail.com>
2023-02-23 12:55:00 -0800

    more nested

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,25 +1,30 @@
-#include "main.h"
-/**
- * print_square - prints a square
- * @size: size of the square
- */
-void print_square(int size)
-{
-	if (size <= 0)
-	{
-		_putchar('\n');
-	}
-	else
-	{
-		int i, j;
+#include <stdio.h>
 
-		for (i = 0; i < size; i++)
-		{
-			for (j = 0; j < size; j++)
-			{
-				_putchar('#');
-			}
-			_putchar('\n');
-		}
+/**
+  *main - entry point
+  *program prints 1 to 100 followed by a new line
+  *multiples of 3 print Fizz
+  *multiples of five print Buzz
+  *multiples of both print FizzBuzz
+  *Return: success
+  */
+
+int main(void)
+{
+	int i = 1;
+
+	for (; i < 100 ; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz ");
+		else if (i % 3 == 0)
+			printf("Fizz ");
+		else if (i % 5 == 0)
+			printf("Buzz ");
+		else
+			printf("%d ", i);
 	}
+	printf("Buzz\n");
+
+	return (0);
 }
