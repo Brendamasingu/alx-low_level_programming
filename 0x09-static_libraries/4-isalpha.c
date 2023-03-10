8e41a08d3123982186fb86786cdd48a1a9667937
Brendamasingu <brendamasingu@gmail.com>
2023-03-10 10:39:00 -0800

    static libraries

--- 0x09-static_libraries/4-isalpha.c
+++ 0x09-static_libraries/4-isalpha.c
@@ -1,15 +1,17 @@
 #include "main.h"
 
 /**
- * _isalpha - checks for alphabets
- * @c: character to be checked
- * Return: 1 if character is a letter, 0 otherwise
+ * _isalpha - checks the alphabetic character
+ * @c: the character to be checked
+ * Return: Returns 1 if c is a letter, lowercase or uppercase else 0
  */
 int _isalpha(int c)
 {
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	if ((c >= 'a' && c <= 'z') ||
+			(c >= 'A' && c <= 'Z'))
+	{
 		return (1);
-	else
-		return (0);
+	}
 
+	return (0);
 }
