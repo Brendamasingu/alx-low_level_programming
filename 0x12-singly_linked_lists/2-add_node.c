003772c15891465e745eedb846256e8cd904baef
brendamasingu <brndamasingu@gmail.com>
2023-04-06 15:13:08 -0700

    task 2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -31,22 +31,3 @@ list_t *add_node(list_t **head, const char *str)
 
 	return (new_node);
 }
-
-/**
- * _strlen - returns the length of a string
- * @s: string
- *
- * Return: length of the string
- */
-unsigned int _strlen(char *s)
-{
-	unsigned int len = 0;
-
-	while (*s != '\0')
-	{
-		len++;
-		s++;
-	}
-
-	return (len);
-}
