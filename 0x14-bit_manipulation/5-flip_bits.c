ccc4928b4164530dd9577355401ef6ae437d7647
brendamasingu <brndamasingu@gmail.com>
2023-04-09 08:54:05 -0700

    task 5

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -5,18 +5,19 @@
  *  to get from one number to another
  * @n: first number
  * @m: second number
- *
  * Return: number of bits to change
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned int count = 0;
-	unsigned long int xor = n ^ m;
+	int i, count = 0;
+	unsigned long int current;
+	unsigned long int exclusive = n ^ m;
 
-	while (xor != 0)
+	for (i = 63; i >= 0; i--)
 	{
-		 count += xor & 1;
-		 xor >>= 1;
+		current = exclusive >> i;
+		if (current & 1)
+			count++;
 	}
 
 	return (count);
